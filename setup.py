28e713218fdbbb92b3039fabd260e56c5efdff39
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-01-20 14:19:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Radhika aggarwal',                   # Type in your name
   author_email = 'raggarwal1_be20@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/Radhika980/Topsis-Radhika',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Radhika980/Topsis-Radhika/new/main',    # I explain this later on
+  download_url = 'https://github.com/Radhika980/Topsis-Radhika/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Easy to Use and Implement', 'Fascinating concept'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
